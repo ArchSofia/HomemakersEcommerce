3728dc9d93dd3127f9d6fe2cd83303ade1dd8f99
ArchSofia <sofiadiazvaldez@gmail.com>
2023-02-11 14:32:05 -0300

    agregue home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import imagen from "/Users/sofiadiazvaldez/Desktop/REACT_CH_2023/proyecto/preEntrega1/src/assets/larusi-havana-bed-linen-1638961198.jpeg";
 
 const Home = () => {
-  return (
-    <div>Bienvenidos a Homemakers</div>
-  )
-}
+	return (
+		<>
+			<img src={imagen} alt="" />
+		</>
+	);
+};
 
-export default Home
+export default Home;
